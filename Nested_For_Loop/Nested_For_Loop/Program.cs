8c9209c4a031e9b8dd05ba5b4cb34789b8ad21e0
Steven Scott <stevescott2653@gmail.com>
2019-09-25 15:51:33 -0400

    Add files via upload

---
+++ Nested_For_Loop/Nested_For_Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine(i+ " "+j);
+                }
+            }
+        }
+    }
+}
